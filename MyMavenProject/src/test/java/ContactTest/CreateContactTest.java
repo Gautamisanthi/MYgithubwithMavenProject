a0be15ae804b54395630030560cfcb4b6192a0a8
gautami1988 <gautami1988@10.164.186.102>
2025-11-14 13:31:47 +0530

    Adding contact and org

---
+++ MyMavenProject/src/test/java/ContactTest/CreateContactTest.java
@@ -0,0 +1,18 @@
+package ContactTest;
+import org.testng.annotations.Test;
+
+
+public class CreateContactTest {
+
+	
+	@Test
+	public void createContactTest()
+	{
+		System.out.println("Execute Create contact");
+	}
+	@Test
+	public void modifyContactTest()
+	{
+		System.out.println("Execute Modify contact");
+	}
+}
---
+++ MyMavenProject/src/test/java/OrgTest/CreateOrgTest.java
@@ -0,0 +1,18 @@
+package OrgTest;
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("Execute create Org Test");
+		
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("Execute modify Org Test");
+		
+	}
+}
